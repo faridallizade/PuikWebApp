c2da2b57ce47c2e535f4e2cf2abac5e39c666920
faridallizade <alizadebuzinez@gmail.com>
2024-06-25 23:35:03 +0400

    Project Already Done

---
+++ PuikWeb/Areas/Admin/DTOs/UpdateProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace WebUI.Areas.Admin.DTOs
+{
+    public class UpdateProductDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public IFormFile File { get; set; }
+        public double Price { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
